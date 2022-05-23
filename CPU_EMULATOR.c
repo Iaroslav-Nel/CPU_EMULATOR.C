359232ccd6aa38b025ec62a1d22c4947fd594038
Iaroslav-Nel <89302587+Iaroslav-Nel@users.noreply.github.com>
2022-05-23 15:26:22 +0300

    Update CPU_EMULATOR.c

--- CPU_EMULATOR.c
+++ CPU_EMULATOR.c
@@ -1,60 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
-int count(char *s2){
+
+int count(char* s2) 
+{
     int k = 0;
-    switch(s2[0]){
-        case 'a': k = 0;
+    switch (s2[0]) 
+    {
+    case 'a': k = 0;
         break;
-        case 'b': k = 1;
+    case 'b': k = 1;
         break;
-        case 'c': k = 2;
+    case 'c': k = 2;
         break;
-        case 'd': k = 3;
+    case 'd': k = 3;
         break;
     }
     return k;
 }
-void sub(int *q, char *s2, int x){
+
+void sub(int* q, char* s2, int x) 
+{
     int k = count(s2);
     q[k] = q[k] - x;
-    for(int i=0; i<4; i++) printf("%d ", q[i]);
+    for (int i = 0; i < 4; i++) 
+        printf("%d ", q[i]);
     return;
 }
-void mov(int *q, char *s2, int x){
+
+void mov(int* q, char* s2, int x) 
+{
     int i = count(s2);
     q[i] = x;
-    for(int i=0; i<4; i++) 
+    for (int i = 0; i < 4; i++)
         printf("%d ", q[i]);
     return;
 }
-void add(int *q, char *s2, int x){
+
+void add(int* q, char* s2, int x) 
+{
     int i = count(s2);
     q[i] = q[i] + x;
-    for(int i = 0; i < 4; i++) 
+    for (int i = 0; i < 4; i++)
         printf("%d ", q[i]);
     return;
 }
+
 int main()
 {
-    int q[4] = {0, 0, 0, 0};
+    int q[4] = { 0, 0, 0, 0 };
     char s1[4], s2[4], s3[4];
     int x;
-    while(1){
+    while (1) 
+    {
         scanf("%s", s1);
         scanf("%s", s2);
         scanf("%s", s3);
-        if((s3[0]=='a') or (s3[0]=='b') or (s3[0]=='c') or (s3[0]=='d')){
-           x = q[count(s3)]; 
+        if ( (s3[0] == 'a') || (s3[0] == 'b') || (s3[0] == 'c') || (s3[0] == 'd') ) 
+        {
+            x = q[count(s3)];
         }
-        else x = s3[0]-48;
-        switch(s1[0]){
-            case 'm': mov(q, s2, x);
+        else x = s3[0] - 48;
+        switch (s1[0]) 
+        {
+        case 'm': mov(q, s2, x);
             break;
-            case 'a': add(q, s2, x);
+        case 'a': add(q, s2, x);
             break;
-            case 's': sub(q, s2, x);
+        case 's': sub(q, s2, x);
             break;
-            case 'r': return 0;
+        case 'r': return 0;
             break;
         }
     }
